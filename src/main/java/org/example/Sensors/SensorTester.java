c1bdb7bdbbda15adc4332a1344aa26db1c6b6a96
miafjensen <miafjensen@gmail.com>
2021-03-12 12:59:31 +0100

    test fra mia

--- src/main/java/org/example/Sensors/SensorTester.java
+++ src/main/java/org/example/Sensors/SensorTester.java
@@ -8,6 +8,6 @@ public class SensorTester {
 
         System.out.println("Fra TS:"+ts.getValue()+" og fra Spo2:"+spO2sensor.getValue());
 
-        //Test fra Mia
+        //Test fra Mia 2
     }
 }
