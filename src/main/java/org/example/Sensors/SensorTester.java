6ee4035afc63344d8653b02521141d1571e67466
EmilieBracht <emiliebracht@gmail.com>
2021-03-12 13:01:28 +0100

    Test Emilie

--- src/main/java/org/example/Sensors/SensorTester.java
+++ src/main/java/org/example/Sensors/SensorTester.java
@@ -9,5 +9,6 @@ public class SensorTester {
         System.out.println("Fra TS:"+ts.getValue()+" og fra Spo2:"+spO2sensor.getValue());
 
         //Test fra Mia 2 og 3
+        //Emilie was here
     }
 }
