a452420d9b7c3b614fbe144fbd586a01f24f9635
miafjensen <miafjensen@gmail.com>
2021-03-23 09:43:10 +0100

    test fra mia 23/3

--- src/main/java/org/example/Sensors/SensorTester.java
+++ src/main/java/org/example/Sensors/SensorTester.java
@@ -7,7 +7,7 @@ public class SensorTester {
         SpO2sensor spO2sensor= new SpO2sensor();
 
         System.out.println("Fra TS:"+ts.getValue()+" og fra Spo2:"+spO2sensor.getValue());
-
+        System.out.println("virker dette?? 23/3");
         //Test fra Mia 2 og 3
         //Emilie was here
     }
