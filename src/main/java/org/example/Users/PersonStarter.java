2425fe38c492f83f12992d410963a79032ac455e
EmilieBracht <emiliebracht@gmail.com>
2021-03-25 21:17:41 +0100

    Users Emilie

---
+++ src/main/java/org/example/Users/PersonStarter.java
@@ -0,0 +1,14 @@
+package org.example.Users;
+
+public class PersonStarter {
+    public static void main(String[] args) {
+        Patient Solveigh=new Patient("Solveigh Nielsen", "123476-1234", "1.68", "KOL");
+        Professional Jens=new Doctor("Jens Hansen", "123", "Neurologisk");
+
+        System.out.println("Kære" + Solveigh.getNavn());
+
+
+    }
+}
+
+}
---
+++ src/main/java/org/example/Users/Professional.java
@@ -0,0 +1,42 @@
+package org.example.Users;
+
+public class Professional {
+    protected String navn;
+    protected int ID;
+    protected int alder;
+    protected String afdeling;
+
+    public String getNavn() {
+        return navn;
+    }
+
+    public void setNavn(String navn) {
+        this.navn = navn;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getAlder() {
+        return alder;
+    }
+
+    public void setAlder(int alder) {
+        this.alder = alder;
+    }
+    public String getAfdeling() {
+        return afdeling;
+    }
+
+    public void setAfdeling(String afdeling) {
+        this.afdeling = afdeling;
+    }
+
+
+}
+
