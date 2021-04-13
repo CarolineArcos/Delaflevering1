097d90626040463bdf53bc001c3ca0e59132e097
miafjensen <miafjensen@gmail.com>
2021-04-13 11:36:57 +0200

    sqlite

---
+++ src/main/java/org/example/database/main.java
@@ -0,0 +1,13 @@
+package org.example.database;
+
+import java.sql.Connection;
+
+public class main {
+
+    public static void main(String[] args) {
+        ConnectionSingleton singleton = new ConnectionSingleton();
+
+        Connection conn = singleton.connectToSQLite("identifier.sqlite");
+    }
+
+}
