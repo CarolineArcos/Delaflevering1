fa0809e9fdcab83e9b5c746a357bb1e4f87364e1
miafjensen <miafjensen@gmail.com>
2021-04-20 10:05:47 +0200

    indsæt i tabel

--- src/main/java/org/example/database/main.java
+++ src/main/java/org/example/database/main.java
@@ -1,15 +1,23 @@
 package org.example.database;
 
+import org.example.Sensors.HRSensor;
+import org.example.Sensors.SpO2sensor;
+import org.example.Sensors.TemperatureSensor;
+
 import java.sql.Connection;
 
 public class main {
+
+
 
     public static void main(String[] args) {
         ConnectionSingleton singleton = new ConnectionSingleton();
 
         Connection conn = singleton.connectToSQLite("identifier.sqlite");
         ConnectionUser cu = new ConnectionUser(conn);
+        //ConnectionMeasurement cm = new ConnectionMeasurement(conn);
         cu.InsertInUsers(160195,"Mia","password", "patient");
+
 
     }
 
