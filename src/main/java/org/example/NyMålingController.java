c5781fe5865204ef753a680b6d1a7e9e06c53465
miafjensen <miafjensen@gmail.com>
2021-04-25 13:02:02 +0200

    med opdatering i app

--- src/main/java/org/example/NyMålingController.java
+++ src/main/java/org/example/NyMålingController.java
@@ -1,21 +1,39 @@
 package org.example;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import org.example.Sensors.HRSensor;
 import org.example.Sensors.SpO2sensor;
 import org.example.Sensors.TemperatureSensor;
+import org.example.database.ConnectionMeasurement;
+import org.example.database.ConnectionSingleton;
+import org.example.database.ConnectionUser;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 
 public class NyMålingController {
 
 
+    public TextField CPR;
+    ScheduledExecutorService event;
+    boolean control = true;
     TemperatureSensor ts = new TemperatureSensor();
     SpO2sensor spO2sensor = new SpO2sensor();
     HRSensor hrSensor = new HRSensor();
+
+    ConnectionSingleton singleton = new ConnectionSingleton();
+    Connection conn = singleton.connectToSQLite("identifier.sqlite");
+    ConnectionMeasurement cm = new ConnectionMeasurement(conn);
 
 
     @FXML
@@ -26,31 +44,66 @@ public class NyMålingController {
     Label dataSpO2;
 
 
-
-
     @FXML
     private synchronized void startSensors() throws InterruptedException {
+        event = Executors.newSingleThreadScheduledExecutor();
+        if (control) {
+            control = false;
+            event.scheduleAtFixedRate(() ->
+                    Platform.runLater(() -> {
+                                double spo2 = spO2sensor.getValue();
+                                double hr = hrSensor.getValue();
+                                double temp = ts.getValue();
 
-            dataSpO2.setText("" + spO2sensor.getValue());
-            dataHR.setText("" + hrSensor.getValue());
-            dataTemp.setText("" + ts.getValue());
-
+                                dataSpO2.setText("" + spo2);
+                                dataHR.setText("" + hr);
+                                dataTemp.setText("" + temp);
+                                int CPRtal = Integer.parseInt(CPR.getText());
+                                int hrTal = (int) hr;
+                                cm.InsertInMeasurements(CPRtal, temp, spo2, hrTal);
+                            }
+                    ), 0, 3, TimeUnit.SECONDS);
+        }
     }
+
     @FXML
     private void switchToNyMåling() throws IOException {
+        if (control == false) {
+            event.shutdown();
+            control = true;
+        }
         App.setRoot("nyMåling");
     }
+
     @FXML
     private void switchToResults() throws IOException {
+        if (control == false) {
+            event.shutdown();
+            control = true;
+        }
         App.setRoot("results");
     }
+
     @FXML
     private void switchToHome() throws IOException {
+        if (control == false) {
+            event.shutdown();
+            control = true;
+        }
         App.setRoot("home");
     }
+
     @FXML
     private void switchToLogIn() throws IOException {
+        if (control == false) {
+            event.shutdown();
+            control = true;
+        }
         App.setRoot("logIn");
     }
 
+    public void stop(ActionEvent actionEvent) {
+        event.shutdown();
+        control = true;
+    }
 }
